cbd7690a84bb97d881481be1136d92a6b78aa49d
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2024-12-17 22:43:46 -0500

    TestDataAnalysis.py
    Just testing the DataAnalysis class

---
+++ TestDataAnalysis.py
@@ -0,0 +1,8 @@
+from DataAnalysis import DataAnalysis
+
+class TestDataAnalysis:
+
+    if __name__ == "__main__":
+            array = [1, 3, 3.02, 4.4, 5.99, 8, 8.04]  # Example array
+            dataSet = DataAnalysis(array)
+            dataSet.checkData()
