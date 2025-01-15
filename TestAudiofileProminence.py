32ca63d1f10873415929a9b9f242015bfe02cc5a
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2025-01-15 07:21:43 -0500

    Add files via upload

---
+++ TestAudiofileProminence.py
@@ -0,0 +1,5 @@
+from AudiofileProminence import AudioFileProminence as afp
+
+if __name__ == "__main__":
+    afp.printAggregateError(directory= r"C:\Users\abeca\OneDrive\ICUNJ_grant_stuff\ICUNJ-grant-audiofiles", numberOfFundamentalsInWindow= 10, percentile= .8, SpecificType="1SRC")
+    afp.findDuplicatesInEntryList("AggError-1SRC-10-0.8.txt" , .5, 2)
