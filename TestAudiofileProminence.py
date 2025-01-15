24dd025f2f2fe620ce2850d96c6f53f4f6190601
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2025-01-15 12:49:10 -0500

    Update TestAudiofileProminence.py

--- TestAudiofileProminence.py
+++ TestAudiofileProminence.py
@@ -2,4 +2,6 @@ from AudiofileProminence import AudioFileProminence as afp
 
 if __name__ == "__main__":
     afp.printAggregateError(directory= r"C:\Users\abeca\OneDrive\ICUNJ_grant_stuff\ICUNJ-grant-audiofiles", numberOfFundamentalsInWindow= 10, percentile= .8, SpecificType="1SRC")
-    afp.findDuplicatesInEntryList("AggError-1SRC-10-0.8.txt" , .5, 2)
+    #afp.findDuplicatesInEntryList("AggError-1SRC-10-0.8.txt" , .5, 2)
+    #print(afp.analyzeTextFile("AggError-1SRC-10-0.8.txt"))
+    afp.findDuplicatesInEntryList("AggError-1SRC-10-0.8.txt", .1, 2)
