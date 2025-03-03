2b5028b95e05959e4c997a955de6251e6fcde939
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2025-03-03 15:27:27 -0500

    Update TestStringAnalysis.py

--- TestStringAnalysis.py
+++ TestStringAnalysis.py
@@ -10,10 +10,15 @@ class TestStringAnalysis:
         length1 = 19.9
         length2= 25.5
         length3= 34.2
-        analysis = StringAnalysis(length1, length2, length3, TSHL.string3TLA10C02())
-        print(analysis.checkIfCotangentSum(maxDifference= .01, minInterval= 0, maxInterval= 7000, numPoints= 100000)[0])
-        print(analysis.checkIfCotangentSum(maxDifference= .01, minInterval= 0, maxInterval= 7000, numPoints= 100000)[1])
+        analysis = StringAnalysis(length1, length2, length3, TSHL.string3TLA10C01())
+        print("cotangent calculations")
+        for entry in (analysis.checkIfCotangentSum(maxDifference= .01, minInterval= 0, maxInterval= 1, numPoints= 100000)[0]):
+            print(entry)
+        print("harmonics")
+        for entry in (analysis.checkIfCotangentSum(maxDifference= .01, minInterval= 0, maxInterval= 1, numPoints= 100000)[1]):
+            print(entry)
         
+        print("3 separate strings")
         for entry in analysis.checkIfStrings(.01):
             print(entry)
 
